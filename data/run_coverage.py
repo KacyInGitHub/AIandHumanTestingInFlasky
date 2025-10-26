cc280de90de0eead84b7a7ede243995540c1f6b8
kacy <kacy@MacBookPro.lan>
2025-10-26 19:16:40 +1300

    move data analysis script to data dir

--- data/run_coverage.py
+++ data/run_coverage.py
@@ -5,7 +5,7 @@ cov = coverage.Coverage(branch=True, include='app/*')
 cov.start()
 
 # 运行测试
-unittest.TextTestRunner().run(unittest.defaultTestLoader.discover('tests/manual'))
+unittest.TextTestRunner().run(unittest.defaultTestLoader.discover('../tests/manual'))
 
 cov.stop()
 cov.save()
